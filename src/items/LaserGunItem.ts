66dd2c1b2235bf70330464e3cf2b9036b295899a
Mizab <mizabmansari@gmail.com>
2023-09-01 21:44:50 +0530

    Added Mag Cooldown

--- src/items/LaserGunItem.ts
+++ src/items/LaserGunItem.ts
@@ -14,8 +14,10 @@ import {
   raw,
   rel,
   tag,
+  title,
 } from "sandstone";
 import { raycast } from "sandstone-raycast";
+import { laserGunPredicateName } from "./CarrotOnAStickBase";
 
 const self = Selector("@s");
 
@@ -25,52 +27,74 @@ const laserGunCooldownObj: ObjectiveInstance = Objective.create(
 );
 const laserGunCooldown = laserGunCooldownObj("@s");
 
+// Burst Cooldown variables
+const timesFiredObj: ObjectiveInstance = Objective.create(
+  "times_fired",
+  "dummy"
+);
+const timesFired = timesFiredObj("@s");
+const burstCooldownObj: ObjectiveInstance = Objective.create(
+  "burst_cooldown",
+  "dummy"
+);
+const burstCooldown = burstCooldownObj("@s");
+const delayBetweenBurst: number = 10 * 20;
+const maxMagSize: number = 15;
+export const excludeUsers: string = "cannot_use_laser_gun";
+
 export const laserGunLogic = MCFunction("items/laser_gun_logic", () => {
-  _.if(laserGunCooldown.matches(0), () => {
-    tag(self).add("used_laser_gun");
-    playsound("minecraft:sfx.laser", "master", "@a", rel(0, 0, 0), 0.1, 1);
-    execute.anchored("eyes").run(() => {
-      raycast(
-        "raycast/laser_gun/main",
-        // @ts-ignore
-        "#aestd1:passthrough",
-        Selector("@e", {
-          type: "#aestd1:living_base",
-          tag: "!used_laser_gun",
-          dx: 0,
-        }),
-        MCFunction("raycast/laser_gun/update", () => {
-          particle(
-            // @ts-ignore
-            "minecraft:dust",
-            [1, 0, 0],
-            1,
-            loc(-0.5, -0.3, 0),
-            [0, 0, 0],
-            1,
-            1
-          );
-        }),
-        MCFunction("raycast/laser_gun/hit", () => {
-          execute
-            .as(
-              Selector("@e", {
-                type: "#aestd1:living_base",
-                tag: "!used_laser_gun",
-                dx: 0,
-              })
-            )
-            .run(() => {
-              raw(`damage @s 3 minecraft:magic`);
-            });
-        }),
-        1,
-        60
-      );
+  _.if(_.not(timesFired.matches([maxMagSize, Infinity])), () => {
+    _.if(laserGunCooldown.matches(0), () => {
+      tag(self).add("used_laser_gun");
+      playsound("minecraft:sfx.laser", "master", "@a", rel(0, 0, 0), 0.1, 1);
+      execute.anchored("eyes").run(() => {
+        raycast(
+          "raycast/laser_gun/main",
+          // @ts-ignore
+          "#aestd1:passthrough",
+          Selector("@e", {
+            type: "#aestd1:living_base",
+            tag: "!used_laser_gun",
+            dx: 0,
+          }),
+          MCFunction("raycast/laser_gun/update", () => {
+            particle(
+              // @ts-ignore
+              "minecraft:dust",
+              [1, 0, 0],
+              1,
+              loc(-0.5, -0.3, 0),
+              [0, 0, 0],
+              1,
+              1
+            );
+          }),
+          MCFunction("raycast/laser_gun/hit", () => {
+            execute
+              .as(
+                Selector("@e", {
+                  type: "#aestd1:living_base",
+                  tag: "!used_laser_gun",
+                  dx: 0,
+                })
+              )
+              .run(() => {
+                raw(`damage @s 3 minecraft:magic`);
+              });
+          }),
+          1,
+          60
+        );
+      });
+      tag(self).remove("used_laser_gun");
+      // Add a cooldown
+      laserGunCooldown.set(10);
+      // Keep track of the times the gun was fired
+      timesFired.add(1);
     });
-    tag(self).remove("used_laser_gun");
-    // Add a cooldown
-    laserGunCooldown.set(10);
+  }).else(() => {
+    burstCooldown.set(delayBetweenBurst);
+    tag(self).add(excludeUsers);
   });
 });
 
@@ -80,6 +104,23 @@ export const laserGunCooldownLogic = () => {
     laserGunCooldown.remove(1);
   }).else(() => {
     laserGunCooldown.set(0);
+  });
+
+  // Burst Cooldown
+  _.if(burstCooldown.matches([0, delayBetweenBurst]), () => {
+    burstCooldown.remove(1);
+    _.if(
+      Selector("@s", { predicate: `default:${laserGunPredicateName}` }),
+      () => {
+        title(self).actionbar({ text: "Cooling Down...", color: "red" });
+      }
+    ).else(() => {
+      title(self).actionbar({ text: "" });
+    });
+  });
+  _.if(burstCooldown.matches(0), () => {
+    timesFired.set(0);
+    tag(self).remove(excludeUsers);
   });
 };
 
